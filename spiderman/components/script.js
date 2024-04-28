81a9b8d4a2dcdf4f2875b4bd1b2dfb187b67f23a
lucas3324k0 <116020130+lucas3324k0@users.noreply.github.com>
2024-04-27 22:13:26 -0300

    Add files via upload

---
+++ spiderman/components/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    console.log('Menu hamburger clicado');
+    document.querySelector('.menu').classList.toggle('active');
+
+});
+
